73086bf77867d9aba716cac6048602c1695676ff
dzgoldman <dzgoldman@wesleyan.edu>
2020-09-10 15:16:16 -0400

    update router address

--- packages/uniswap-interface/src/constants/index.ts
+++ packages/uniswap-interface/src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x610e322b48A6DA912641CfC9ebcd5BFfFaaFEf69'
 
 // a list of tokens by chain
 type ChainTokenList = {
