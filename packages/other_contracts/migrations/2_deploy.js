95baf741bedaf5e7e188f79f59e9197f011592e9
dzgoldman <dzgoldman@wesleyan.edu>
2020-09-21 18:07:33 -0400

    update deploy script

--- packages/other_contracts/migrations/2_deploy.js
+++ packages/other_contracts/migrations/2_deploy.js
@@ -2,7 +2,7 @@ const WETH9 = artifacts.require("WETH9")
 const MULTICALL = artifacts.require("Multicall")
 const ROUTER  = artifacts.require("UniswapV2Router02")
 const fs = require('fs')
-const contracts = require('../../contract_addresses.json')
+const contracts = require('../../uniswap-sdk/contract_addresses.json')
 module.exports = async function(deployer) {
     await deployer.deploy(MULTICALL);
     const multicallContract = MULTICALL.deployed()
@@ -11,7 +11,7 @@ module.exports = async function(deployer) {
 
     await deployer.deploy(ROUTER, contracts.factoryAddress, weth9Contract.address);
     const  routerContract = await ROUTER.deployed()
-    fs.writeFileSync('../../packages/contract_addresses.json',JSON.stringify({
+    fs.writeFileSync('../../packages/uniswap-sdk/contract_addresses.json',JSON.stringify({
       ...contracts,
       wethAddress: weth9Contract.address,
       routerContract: routerContract.address,
