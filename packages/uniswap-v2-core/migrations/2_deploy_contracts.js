0760b36ce99dd69c5010e83cc622719912e4c36e
dzgoldman <dzgoldman@wesleyan.edu>
2020-09-22 14:43:22 -0400

    add uniswap erc20 to deploy

--- packages/uniswap-v2-core/migrations/2_deploy_contracts.js
+++ packages/uniswap-v2-core/migrations/2_deploy_contracts.js
@@ -1,10 +1,13 @@
 const UniswapV2Factory = artifacts.require("UniswapV2Factory");
+const UniswapV2ERC20 = artifacts.require("UniswapV2ERC20");
 
 const fs = require('fs')
 const contracts = require('../../uniswap-sdk/contract_addresses.json')
 
 module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(UniswapV2Factory, accounts[0]);
+  await deployer.deploy(UniswapV2ERC20);
+
   const factoryContract = await UniswapV2Factory.deployed()
   fs.writeFileSync('../../packages/uniswap-sdk/contract_addresses.json',JSON.stringify({
     ...contracts,
