2285cb747850121195ac92b4349f31dfd2d39259
Harry Kalodner <harry@offchainlabs.com>
2020-09-23 15:37:48 -0400

    Update uniswapv2pair init code hash

--- packages/other_contracts/contracts/libraries/UniswapV2Library.sol
+++ packages/other_contracts/contracts/libraries/UniswapV2Library.sol
@@ -21,7 +21,7 @@ library UniswapV2Library {
                 hex'ff',
                 factory,
                 keccak256(abi.encodePacked(token0, token1)),
-                hex'96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f' // init code hash
+                hex'9259ac0d6fac88c367806e7e156aeea5dfe97700ff316ef39980486f55cd3261' // init code hash
             ))));
     }
 
