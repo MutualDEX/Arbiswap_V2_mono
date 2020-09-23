f1b1dd9aa43b5abafcdf6319a876e17b25fd7333
Harry Kalodner <harry@offchainlabs.com>
2020-09-23 15:38:22 -0400

    Enable local development deployment

--- packages/other_contracts/truffle-config.js
+++ packages/other_contracts/truffle-config.js
@@ -41,6 +41,11 @@ module.exports = {
    */
 
   networks: {
+    development: {
+      host: '127.0.0.1',
+      port: 8545,
+      network_id: '*', // Match any network id
+    },
 
     remote_arbitrum: {
       provider: function () {
