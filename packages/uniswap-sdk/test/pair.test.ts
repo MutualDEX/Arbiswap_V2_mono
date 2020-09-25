aad6dff95118f24fe9c09751e573268389c715f7
dzgoldman <dzgoldman@wesleyan.edu>
2020-09-25 14:43:30 -0400

    update sdk constants tests

--- packages/uniswap-sdk/test/pair.test.ts
+++ packages/uniswap-sdk/test/pair.test.ts
@@ -14,7 +14,7 @@ describe('Pair', () => {
 
   describe('#getAddress', () => {
     it('returns the correct address', () => {
-      expect(Pair.getAddress(USDC, DAI)).toEqual('0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5')
+      expect(Pair.getAddress(USDC, DAI)).toEqual('0xF222716305825D05f3fC7D46969D3840B6FF68de')
     })
   })
 
