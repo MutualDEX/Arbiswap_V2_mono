49432e0f6b10b55a319cd690d75a1b24265fb406
dzgoldman <dzgoldman@wesleyan.edu>
2020-10-23 09:51:43 -0400

    fix casing in token list json file path

--- packages/uniswap-interface/src/constants/lists.ts
+++ packages/uniswap-interface/src/constants/lists.ts
@@ -1,5 +1,5 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = `${window.location.origin}/arbtokenlist.json`
+export const DEFAULT_TOKEN_LIST_URL = `${window.location.origin}/arbTokenList.json`
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
