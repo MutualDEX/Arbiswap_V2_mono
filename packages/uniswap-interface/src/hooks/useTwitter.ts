91a29729e6bec73e6c5bbfcad4d555037330f1fe
dzgoldman <dzgoldman@wesleyan.edu>
2020-11-05 20:18:29 -0500

    make "your address goes here" more obvious

--- packages/uniswap-interface/src/hooks/useTwitter.ts
+++ packages/uniswap-interface/src/hooks/useTwitter.ts
@@ -2,7 +2,7 @@ import { useActiveWeb3React } from '../hooks'
 
  const useTwitter = ()=>{
     const { account } = useActiveWeb3React()
-    const text = `.@Arbi_Swap hey @OffchainLabs, gimme some Arbiswap test tokens plz! https://swap.arbitrum.io/  ${account || '0xyouraddresshere'}`
+    const text = `.@Arbi_Swap hey @OffchainLabs, gimme some Arbiswap test tokens plz! https://swap.arbitrum.io/  ${account || '0x*YOUR**ETH**ADDRESS**GOES**HERE**'}`
       .split(' ')
       .join('%20')
     const handleClick = () => {
